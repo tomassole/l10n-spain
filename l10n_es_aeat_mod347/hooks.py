4c8ca8a4330df6e5fa063765bd19ea1a1435e747
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-05 12:53:46 +0200

    [MIG] l10n_es_aeat_mod347: Migration to 10.0

--- l10n_es_aeat_mod347/hooks.py
+++ l10n_es_aeat_mod347/hooks.py
@@ -3,7 +3,7 @@
 #             (http://www.serviciosbaeza.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import SUPERUSER_ID
+from odoo import SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):

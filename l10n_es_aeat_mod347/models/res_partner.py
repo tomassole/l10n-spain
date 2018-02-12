5d05595fe3af6900d8418bf423cfee8e050ed92c
javierjcf <javierjcf@gmail.com>
2018-02-12 18:15:13 +0100

    [MIG]l10n_es_mod_347: Envío masivo mail MIGRACIÓN V10

--- l10n_es_aeat_mod347/models/res_partner.py
+++ l10n_es_aeat_mod347/models/res_partner.py
@@ -6,12 +6,12 @@ from odoo import fields, models, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-      @api.multi
-     def _compute_num_347_records(self):
-          record_obj = self.env['l10n.es.aeat.mod347.partner_record']
-          for partner in self:
-              cond = [('partner_id', '=', partner.id)]
-              partner.num_347_records = len(record_obj.search(cond))
+    @api.multi
+    def _compute_num_347_records(self):
+        record_obj = self.env['l10n.es.aeat.mod347.partner_record']
+        for partner in self:
+            cond = [('partner_id', '=', partner.id)]
+            partner.num_347_records = len(record_obj.search(cond))
 
     not_in_mod347 = fields.Boolean(
         "Not included in 347 report",
@@ -30,14 +30,14 @@ class ResPartner(models.Model):
 
     @api.multi
     def show_partner_347_records(self):
-    res = {'view_mode': 'tree,form',
-           'res_model': 'l10n.es.aeat.mod347.partner_record',
-           'view_id': False,
-           'type': 'ir.actions.act_window',
-           'view_type': 'form',
-           'domain': [('partner_id', '=', self.id)],
-           'context': {'tree_view_ref': 'l10n_es_aeat_mod347.l10n_es_aeat_'
-                       'mod347_partner_record_tree_view'}}
+        res = {
+            'view_mode': 'tree,form',
+            'res_model': 'l10n.es.aeat.mod347.partner_record',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'domain': [('partner_id', '=', self.id)],
+            'context':
+                {'tree_view_ref': 'l10n_es_aeat_mod347.l10n_es_aeat_'
+                 'mod347_partner_record_tree_view'}}
         return res
-
-
